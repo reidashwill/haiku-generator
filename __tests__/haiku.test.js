609b3580eab0231c3c89bd89f9a60240958ac31f
Reid Ashwill <reidashwill@gmail.com>
2020-04-15 10:31:26 -0700

    troubleshoot syllableChecker

--- __tests__/haiku.test.js
+++ __tests__/haiku.test.js
@@ -3,7 +3,16 @@ import { Haiku } from "./../src/haiku.js"
 describe("Haiku", () => {
   
   test('Should verify that object only contains strings', () => {
-    const haiku = new Haiku("this is a test", "so is this", "this is yet another test string")
-    expect(typeOf(haiku.line1).toEqual("string"))
+    const haiku = new Haiku("this is a string", "so is this", "this is yet another test string");
+    expect(typeof(haiku.line1)).toEqual("string");
+    expect(typeof(haiku.line2)).toEqual("string");
+    expect(typeof(haiku.line3)).toEqual("string");
+  })
+
+  test('should check to see if line1 contains 5 syllables', () => {
+    const newHaiku = new Haiku("this is a string", "so is this", "this is another string");
+    expect(newHaiku.syllableChecker(newHaiku.line1)).toEqual(4);
+    expect(newHaiku.syllableChecker(newHaiku.line2)).toEqual(3);
+    expect(newHaiku.syllableChecker(newHaiku.line3)).toEqual(6);
   })
 })
--- src/haiku.js
+++ src/haiku.js
@@ -4,4 +4,11 @@ export class Haiku {
     this.line2 = line2;
     this.line3 = line3;
   }
+
+  syllableChecker(line1, line2, line3) {
+    line1 = line1.toLowerCase();                                     
+    if(line1.length <= 3) { return 1; }                            
+      line1 = line1.replace(/([^laeiouy]es|ed)$/, ''); 
+      line1 = line1.replace(/^y/, '');
+      return line1.match(/[aeiouy]{1,2}/g).length;
 }
