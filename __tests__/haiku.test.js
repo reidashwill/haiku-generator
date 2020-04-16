20c3110fa4b819516e76c7087637bc0df86d5b3a
Reid Ashwill <reidashwill@gmail.com>
2020-04-16 09:08:15 -0700

    finish testing individual morning parts for app

--- __tests__/haiku.test.js
+++ __tests__/haiku.test.js
@@ -2,27 +2,22 @@ import { Haiku } from "./../src/haiku.js"
 
 describe("Haiku", () => {
   
-  // test('Should verify that object only contains strings', () => {
-  //   const haiku = new Haiku("this is a string", "so is this", "this is yet another test string");
-  //   expect(typeof(haiku.line1)).toEqual("string");
-  //   expect(typeof(haiku.line2)).toEqual("string");
-  //   expect(typeof(haiku.line3)).toEqual("string");
-  // })
+  test('Should verify that object only contains strings', () => {
+    const haiku = new Haiku("this is a string", "so is this", "this is yet another test string");
+    expect(typeof(haiku.line1)).toEqual("string");
+    expect(typeof(haiku.line2)).toEqual("string");
+    expect(typeof(haiku.line3)).toEqual("string");
+  })
 
-  // test('check for syllables in individual words', () => {
-  //   const newHaiku = new Haiku("soothed", "yourselves", "funkingroovin");
-  //   expect(newHaiku.syllableChecker(newHaiku.line1)).toEqual(1);
-  //   expect(newHaiku.syllableChecker(newHaiku.line2)).toEqual(2);
-  //   expect(newHaiku.syllableChecker(newHaiku.line3)).toEqual(4);
-  // })
+  test('check for syllables in individual words', () => {
+    const newHaiku = new Haiku("soothed", "yourselves", "funkingroovin");
+    expect(newHaiku.syllableChecker(newHaiku.line1)).toEqual(1);
+    expect(newHaiku.syllableChecker(newHaiku.line2)).toEqual(2);
+    expect(newHaiku.syllableChecker(newHaiku.line3)).toEqual(4);
+  })
 
-  // test("should determine if inputs are being changed into arrays" , () => {
-  //   let haiku2 = new Haiku("soothed is a string", "yourselves is also a string", "funkingroovin as well is a string my friends");
-  //   expect(haiku2.stringBreaker().length).toEqual(3);
-  //   } )
-
-    test("should return numbers of syllables in each index", () => {
-      let testVar = new Haiku (["test", "number", "funkingroovin"],[""],[""])
-      expect(testVar.testFunction()).toEqual(7);
-    })
+  test("should determine if inputs are being changed into arrays" , () => {
+    let haiku2 = new Haiku("soothed is a string", "yourselves is also a string", "funkingroovin as well is a string my friends");
+    expect(haiku2.stringBreaker().length).toEqual(3);
+    } )
   })
--- src/haiku.js
+++ src/haiku.js
@@ -5,37 +5,29 @@ export class Haiku {
     this.line3 = line3;
   }
 
-  testFunction() {
-    this.line1.forEach(function() {
-      this.syllableChecker(this.line1);
-    });
-  }
 
-  // stringBreaker() {
-  //   var haikuArr = [this.line1, this.line2, this.line3];
-  //   haikuArr[0].forEach(function(){
+  stringBreaker() {
+    var haikuArr = [this.line1, this.line2, this.line3];
+    // haikuArr[0].forEach(function(){
       
       
-  //   })
-  //   var newArray = [];
-  //   for (let i = 0; i < haikuArr.length; i++){
-  //     newArray.push(haikuArr[i].split(" "));
-  //    }
-  //   return newArray;
-  // }
+    // })
+    var newArray = [];
+    for (let i = 0; i < haikuArr.length; i++){
+      newArray.push(haikuArr[i].split(" "));
+     }
+    return newArray;
+  }
   
-  syllableChecker(){
-    // stringBreaker(line1, line2, line3);
-    // for(let i = 0; i < newArray.length; i ++) {
-    //   for(let i =0; i < newArray[i].length; i++){
-        // let word = newArray[i].toLowerCase();  
-        if(this.line1.length <= 3) { 
-          return 1; //ether
-        }else{
-          this.line1 = this.line1.replace(/([^laeiouy]es|ed)$/i, '');
-          this.line1 = this.line1.replace(/([.!?:;])$/i, '');
-          this.line1 = this.line1.replace(/^y/i, '');
-          return this.line1.match(/[aeiouy]{1,2}/gi).length; 
-        }
+  syllableChecker(passedWord){
+    let word = passedWord.toLowerCase();  
+    if(word.length <= 3) { 
+      return 1; //ether
+    }else{
+      word = word.replace(/([^laeiouy]es|ed)$/i, '');
+      word = word.replace(/([.!?:;])$/i, '');
+      word = word.replace(/^y/i, '');
+      return word.match(/[aeiouy]{1,2}/gi).length; 
+    }
   }
 };
